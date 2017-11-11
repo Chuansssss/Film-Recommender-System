140b25f700e4c231636f7afa209ade02f85c9427
J.C. Shang <jinchuas@usc.edu>
2017-11-10 16:39:53 -0800

    Update DataDividerByUser.java

--- src/main/java/DataDividerByUser.java
+++ src/main/java/DataDividerByUser.java
@@ -13,19 +13,6 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class DataDividerByUser {
 	public static class DataDividerMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
-
-		// map method
-//		@Override
-//		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-//
-//			//input user,movie,rating
-//			String[] user_movie_rating = value.toString().trim().split(",");
-//			int userID = Integer.parseInt(user_movie_rating[0]);
-//			String movieID = user_movie_rating[1];
-//			String rating = user_movie_rating[2];
-//
-//			context.write(new IntWritable(userID), new Text(movieID + ":" + rating));
-//		}
 		@Override
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String[] user_movie_rating = value.toString().trim().split(",");
@@ -38,18 +25,6 @@ public class DataDividerByUser {
 	}
 
 	public static class DataDividerReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
-		// reduce method
-//		@Override
-//		public void reduce(IntWritable key, Iterable<Text> values, Context context)
-//				throws IOException, InterruptedException {
-//
-//			StringBuilder sb = new StringBuilder();
-//			while (values.iterator().hasNext()) {
-//				sb.append("," + values.iterator().next());
-//			}
-//			//key = user value=movie1:rating, movie2:rating...
-//			context.write(key, new Text(sb.toString().replaceFirst(",", "")));
-//		}
 		@Override
 		public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 			StringBuilder sb = new StringBuilder();
